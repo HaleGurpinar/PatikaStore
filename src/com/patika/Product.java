408c11c0271cd6dad77dd2f6977741f9a7fd896c
Hale <itshalenurgurpinar@gmail.com>
2023-08-01 17:49:55 +0300

    product feautures

--- src/com/patika/Product.java
+++ src/com/patika/Product.java
@@ -1,4 +1,158 @@
 package com.patika;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Product {
+
+    static Scanner scanner= new Scanner(System.in);
+    private int id;
+    private int idPhone;
+    private int idNotebook;
+    private double price;
+    private double discountRate;
+    private int stock;
+    private String name;
+    private String brand;
+    private int memory;
+    private double screen;
+    private double battery;
+    private double ram;
+    private String color;
+    private String productName;
+
+    private static HashMap<String, ArrayList<Product>> products = new HashMap<>() {
+        {
+            put("Notebook", new ArrayList<Product>());
+            put("MobilePhone", new ArrayList<Product>());
+        }
+    };
+
+    public Product(int idPhone, int idNotebook, double price, double discountRate, int stock, String name, String brand, int memory, double screen, double battery, double ram, String color) {
+        this.idPhone = idPhone;
+        this.idNotebook = idNotebook;
+        this.price = price;
+        this.discountRate = discountRate;
+        this.stock = stock;
+        this.name = name;
+        this.brand = brand;
+        this.memory = memory;
+        this.screen = screen;
+        this.battery = battery;
+        this.ram = ram;
+        this.color = color;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public  int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdPhone() {
+        return idPhone;
+    }
+
+    public void setIdPhone(int idPhone) {
+        this.idPhone = idPhone;
+    }
+
+    public  int getIdNotebook() {
+        return idNotebook;
+    }
+
+    public void setIdNotebook(int idNotebook) {
+        this.idNotebook = idNotebook;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public double getScreen() {
+        return screen;
+    }
+
+    public void setScreen(double screen) {
+        this.screen = screen;
+    }
+
+    public double getBattery() {
+        return battery;
+    }
+
+    public void setBattery(double battery) {
+        this.battery = battery;
+    }
+
+    public double getRam() {
+        return ram;
+    }
+
+    public void setRam(double ram) {
+        this.ram = ram;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
